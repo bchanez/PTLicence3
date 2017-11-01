aecbe84439f792dbf29bc6d81d4f15570ac832b7
bchanez <bastien.chanez@gmail.com>
2017-11-01 22:56:17 +0100

    realisation de la methode add_resourceDirectory et remove_resourceDirectory

--- src/ResourceManager/CResource_Manager.cpp
+++ src/ResourceManager/CResource_Manager.cpp
@@ -16,10 +16,23 @@
 
 void CResource_Manager::add_resourceDirectory (const std::string& directory)
 {
+  // check if the directory already exist else add it
+  for (std::vector<std::string>::iterator it = m_resourceDirectories.begin(); it != m_resourceDirectories.end(); ++it)
+    if (directory.compare(*it) == 0)
+      return;
 
+  // add the directory
+  m_resourceDirectories.push_back(directory);
 }
 
 void CResource_Manager::remove_resourceDirectory (const std::string& directory)
 {
-
+  // check if the directory exist and delete it
+  for (std::vector<std::string>::iterator it = m_resourceDirectories.begin(); it != m_resourceDirectories.end(); ++it)
+    if (directory.compare(*it) == 0)
+    {
+      // delete the directory
+      m_resourceDirectories.erase(it);
+      return;
+    }
 }
