a6dc98129a0243d40eb3066fb63f16dd91bc33f4
Yohann Gentelet <sgt.nosky@gmail.com>
2017-11-16 12:21:46 +0100

    main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -8,15 +8,6 @@ int main()
 {
     RenderWindow window(VideoMode(200, 200), "Projet");
 
-<<<<<<< HEAD
-    Texture texture;
-    if (!texture.loadFromFile("rsc/graphics/character.png"))
-    {
-        cout << "Impossible d'ouvrir l'image : character.png";
-    }
-
-=======
->>>>>>> e667c25615f1a0d059a45d63410953f48df2b306
     Sprite sprite;
     sprite.setTexture(CResourceHolder::get().texture(ETexture_Name::e_Characters));
 
