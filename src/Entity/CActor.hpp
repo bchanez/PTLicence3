4f5fb79c1b93bf7475df9155614e1fac147b8ce1
bchanez <bastien.chanez@gmail.com>
2017-12-28 13:40:06 +0100

    creation de la classe CActor

---
+++ src/Entity/CActor.hpp
@@ -0,0 +1,35 @@
+#pragma once
+
+#include "CEntity.hpp"
+#include "../ResourceManager/CResourceHolder.hpp"
+#include "../Tools/DEBUG.hpp"
+#include "../Tools/CDisplay.hpp"
+#include "../Tools/CRandom.hpp"
+
+class CActor : public CEntity
+{
+    struct Key { bool left, right, up, down, shift; };
+
+  public:
+
+    explicit CActor(bool isCharacter);
+    virtual ~CActor(void);
+
+    void setTexture(void);
+    void setAnimation(void) override;
+    void input(sf::Event * event);
+    void update(float dt) override;
+
+  private:
+
+    bool m_isCharacter;
+
+    Key m_key;
+    enum Estate {e_idle, e_walk, e_run, e_action, e_dead, e_wander, e_question, e_disappear};
+
+    enum EAnimation {e_walk_right, e_walk_left};
+    unsigned int m_nb_animation = 2;
+
+    sf::Vector2i m_goal_point;
+    sf::Vector2f m_stop;
+};
