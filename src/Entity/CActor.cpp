04fd38a5e393b326e5bdf4d12b81a9bee85bbb76
bchanez <bastien.chanez@gmail.com>
2017-12-29 15:24:01 +0100

    correction

--- src/Entity/CActor.cpp
+++ src/Entity/CActor.cpp
@@ -248,11 +248,7 @@ void CActor::input(sf::Event * event)
   {
     m_key.left = m_key.right = m_key.up = m_key.down = m_key.shift = false;
 
-<<<<<<< HEAD
-    if (m_goal_point.x != m_position.x || m_goal_point.y != m_position.y) // si point de destination
-=======
     if ((m_goal_point.x != (int) m_position.x || m_goal_point.y != (int) m_position.y) && m_goal_point != sf::Vector2i(0, 0)) // si point de destination
->>>>>>> fbdc34949c70dee24f53773047139f5f5c64e238
     {
       if(m_goal_point.x < (int) m_position.x)
         m_key.left = CRandom::intInRange(0, 20) != 0;
@@ -346,12 +342,7 @@ void CActor::update(float dt)
           m_state = e_run;
 
 
-<<<<<<< HEAD
-      //if (m_position != (sf::Vector2i) m_sprite.getPosition())
-      if (m_goal_point.x != m_position.x || m_goal_point.y != m_position.y)
-=======
       if (m_position != m_sprite.getPosition())
->>>>>>> fbdc34949c70dee24f53773047139f5f5c64e238
       {
         // mise a jour de la position
         m_sprite.setPosition(m_position);
