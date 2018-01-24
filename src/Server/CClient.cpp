095e33c03b664e94b42009759ac5da674c8fd52f
Yohann Gentelet <sgt.nosky@gmail.com>
2018-01-24 11:18:06 +0100

    .

--- src/Server/CClient.cpp
+++ src/Server/CClient.cpp
@@ -6,8 +6,8 @@
   LOG("CClient constructor\n");
   m_UDPserver.bind(55003);
 
-  //m_serveur = "192.168.43.49";
-  m_serveur = "localhost";
+  m_serveur = "192.168.43.26";
+  //m_serveur = "localhost";
 }
 
 /*virtual*/ CClient::~CClient(void)
